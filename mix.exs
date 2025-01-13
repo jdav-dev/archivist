b5d0ad9a7f6149b8920b36f5e097d0164fec308d
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2025-01-13 09:01:27 -0500

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Archivist.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :archivist,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Archivist.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ollama, "~> 0.8.0"}
+    ]
+  end
+end
